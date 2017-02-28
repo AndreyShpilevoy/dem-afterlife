ef95f4e9243b8a0fb3b63a527387a116923d6834
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-02-28 12:26:04 +0200

    refactored ctyle file for Header component

--- src/dem-afterlife-ui/src/scripts/components/Header/calculateStyles.js
+++ src/dem-afterlife-ui/src/scripts/components/Header/calculateStyles.js
@@ -1,40 +1,33 @@
-const calculateStyles = ({grid, header}) => {
-    return grid.containers.reduce((previouse, current) => {
-        let result;
-        if (current.size !== 'xs' && current.size !== 'sm') {
-            result = Object.assign({}, previouse, {
-                [ `@media (${ current.min })` ]: {
-                    header: {
-                        backgroundImage: `url(${ header[ `${ current.size }` ].backgroundImage })`,
-                        height: header[ current.size ].height,
-                        width: '100%'
-                    },
-                    headerPadding: {
-                        paddingTop: header[ current.size ].height,
-                    }
-                }
-            });
-        } else {
-            result = Object.assign({}, previouse, {
-                [ `@media (${ current.min })` ]: {
-                    header: {
-                        height: header[ current.size ].height,
-                        width: '100%'
-                    },
-                    headerPadding: {
-                        paddingTop: header[ current.size ].height,
-                    }
-                }
-            });
-        }
-        return result;
-    },
-        {
-            fixedOnTheTop: {
-                position: 'fixed',
-                top: 0
-            }
-        });
+const initialModel = {
+    fixedOnTheTop: {
+        position: 'fixed',
+        top: 0
+    }
 };
+
+const template = (size, min, header) => {
+    let model = {
+        [ `@media (${ min })` ]: {
+            header: {
+                height: header[ size ].height,
+                width: '100%'
+            },
+            headerPadding: {
+                paddingTop: header[ size ].height,
+            }
+        }
+    };
+    if (size !== 'xs' && size !== 'sm') {
+        model[ `@media (${ min })` ].header = Object.assign({}, model[ `@media (${ min })` ].header,
+            {
+                backgroundImage: `url(${ header[ `${ size }` ].backgroundImage })`
+            });
+    }
+    return model;
+};
+
+const calculateStyles = ({grid, header}) =>
+    grid.containers.reduce((previouse, {size, min}) =>
+        Object.assign({}, previouse, template(size, min, header)), initialModel);
 
 export default calculateStyles;
