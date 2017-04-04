f1153d51d9334e201af2b30381945fe42c4e0a4a
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-04-05 00:17:55 +0300

    fixed typos in Hidden calculateStyle

--- src/dem-afterlife-ui/src/scripts/components/Hidden/calculateStyles.js
+++ src/dem-afterlife-ui/src/scripts/components/Hidden/calculateStyles.js
@@ -4,7 +4,7 @@ const calculateStyles = ({grid}) =>
     grid.containers.reduce((previouse, {gridSize, mediaMinString, mediaMaxString}) => ({
         ...previouse,
         ...createMediaQueryMin(mediaMinString, {[`hidden-up-${gridSize}`]: {display: 'none'} }),
-        ...createMediaQueryMin(createMediaQueryMax, {[`hidden-down-${gridSize}`]: {display: 'none'} }),
+        ...createMediaQueryMax(mediaMaxString, {[`hidden-down-${gridSize}`]: {display: 'none'} }),
         ...createMediaQueryMinMax(mediaMinString, mediaMaxString, {[`hidden-exact-${gridSize}`]: {display: 'none'} })
     }), {});
 
