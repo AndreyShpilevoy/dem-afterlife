6f5e577d27300d2d06f9d78bde593074a247efe7
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-04-12 00:43:31 +0300

    change statefull Conference component to statless

--- src/dem-afterlife-ui/src/scripts/containers/Conference/index.js
+++ src/dem-afterlife-ui/src/scripts/containers/Conference/index.js
@@ -1,21 +1,13 @@
-/* eslint fp/no-class: 0, fp/no-nil: 0, fp/no-unused-expression: 0, fp/no-mutation: 0, fp/no-this: 0*/
-
 import React from 'react';
 import {node} from 'prop-types';
 import {connect} from 'react-redux';
 import Presentation from './Presentation';
 
-class Conference extends React.Component {
-    static propTypes = {
-        children: node
-    };
+const Conference = ({children}) =>
+    <Presentation children={children}/>;
 
-    render() {
-        return (
-      <Presentation
-          children={this.props.children}/>
-        );
-    }
-}
+Conference.propTypes = {
+    children: node
+};
 
 export default connect()(Conference);
