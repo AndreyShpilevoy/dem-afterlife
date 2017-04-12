81dc319819d796a3e28c804e6fa492df8bd1ab2d
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-04-12 19:26:09 +0300

    added tests for Notification reducer

--- src/dem-afterlife-ui/src/scripts/containers/Notification/notification-reducer.test.js
+++ src/dem-afterlife-ui/src/scripts/containers/Notification/notification-reducer.test.js
@@ -1,18 +1,21 @@
 /* eslint no-undef: 0, fp/no-unused-expression: 0, fp/no-nil: 0, fp/no-mutation: 0*/
 
-import notificationReducer, {
+import {
+    notificationReducer,
     addNotification,
     addInfoNotification,
     addSuccessNotification,
     addWarningNotification,
     addExceptionNotification,
-    removeNotification
+    removeNotification,
+    ADD_NOTIFICATION,
+    REMOVE_NOTIFICATION
 } from './notification-reducer';
 
 Date.now = jest.genMockFunction().mockReturnValue(1492003118322);
 
 describe('Notification reducer', () => {
-    it('addNotification should create expected event', () => {
+    it('addNotification should create expected object', () => {
         const notification = {
             message: 'message',
             title: 'title',
@@ -30,7 +33,7 @@ describe('Notification reducer', () => {
         expect(addNotification(notification, 'info')).toEqual(expectedResult);
     });
 
-    it('addInfoNotification should create expected event', () => {
+    it('addInfoNotification should create expected object', () => {
         const notification = {
             message: 'message',
             title: 'title',
@@ -48,7 +51,7 @@ describe('Notification reducer', () => {
         expect(addInfoNotification(notification)).toEqual(expectedResult);
     });
 
-    it('addSuccessNotification should create expected event', () => {
+    it('addSuccessNotification should create expected object', () => {
         const notification = {
             message: 'message',
             title: 'title',
@@ -66,7 +69,7 @@ describe('Notification reducer', () => {
         expect(addSuccessNotification(notification)).toEqual(expectedResult);
     });
 
-    it('addWarningNotification should create expected event', () => {
+    it('addWarningNotification should create expected object', () => {
         const notification = {
             message: 'message',
             title: 'title',
@@ -84,7 +87,7 @@ describe('Notification reducer', () => {
         expect(addWarningNotification(notification)).toEqual(expectedResult);
     });
 
-    it('addExceptionNotification should create expected event', () => {
+    it('addExceptionNotification should create expected object', () => {
         const notification = {
             message: 'message',
             title: 'title',
@@ -102,11 +105,79 @@ describe('Notification reducer', () => {
         expect(addExceptionNotification(notification)).toEqual(expectedResult);
     });
 
-    it('removeNotification should create expected event', () => {
+    it('removeNotification should create expected object', () => {
         const expectedResult = {
             type: 'REMOVE_NOTIFICATION',
             payload: {uid: 1492003118322}
         };
         expect(removeNotification(1492003118322)).toEqual(expectedResult);
+    });
+
+    it('notificationReducer with action ADD_NOTIFICATION should return expected state', () => {
+        const defaulState = {
+            notifications: []
+        };
+        const action = {
+            type: ADD_NOTIFICATION,
+            payload: {
+                notification: {
+                    message: 'message',
+                    title: 'title',
+                    autoDismiss: 5,
+                    level: 'info',
+                    uid: 1492003118322} }
+        };
+        const expectedResult = {
+            notifications: [{
+                autoDismiss: 5,
+                level: 'info',
+                message: 'message',
+                title: 'title',
+                uid: 1492003118322
+            }] };
+        expect(notificationReducer(defaulState, action)).toEqual(expectedResult);
+    });
+
+    it('notificationReducer with action REMOVE_NOTIFICATION should return expected state', () => {
+        const defaulState = {
+            notifications: [{
+                autoDismiss: 5,
+                level: 'info',
+                message: 'message',
+                title: 'title',
+                uid: 1492003118322
+            }] };
+        const action = {
+            type: REMOVE_NOTIFICATION,
+            payload: {uid: 1492003118322}
+        };
+        const expectedResult = {
+            notifications: []
+        };
+        expect(notificationReducer(defaulState, action)).toEqual(expectedResult);
+    });
+
+    it('notificationReducer with invald action should return expected state', () => {
+        const defaulState = {
+            notifications: [{
+                autoDismiss: 5,
+                level: 'info',
+                message: 'message',
+                title: 'title',
+                uid: 1492003118322
+            }] };
+        const action = {
+            type: 'wrong',
+            payload: {}
+        };
+        const expectedResult = {
+            notifications: [{
+                autoDismiss: 5,
+                level: 'info',
+                message: 'message',
+                title: 'title',
+                uid: 1492003118322
+            }] };
+        expect(notificationReducer(defaulState, action)).toEqual(expectedResult);
     });
 });
