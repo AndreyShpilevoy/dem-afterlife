2ec79cae15be4bd18ceb5c4bdd18373c195f8758
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-05-29 23:00:49 +0300

    added tests for EN dateTimeDeclension

---
+++ src/dem-afterlife-ui/src/scripts/utils/localization/translations/dateTimeDeclensions/en.test.js
@@ -0,0 +1,13 @@
+/* eslint no-undef: 0, fp/no-unused-expression: 0, fp/no-nil: 0*/
+
+import dateTimeDeclension from './en';
+
+describe('EN dateTimeDeclension', () => {
+    it('dateTimeDeclension return singleForm string for 1 item', () => {
+        expect(dateTimeDeclension(1, 'firstForm', 'secondForm', 'trirdForm', 'singleForm')).toEqual('singleForm');
+    });
+
+    it('dateTimeDeclension return secondForm string for 2 item', () => {
+        expect(dateTimeDeclension(2, 'firstForm', 'secondForm', 'trirdForm', 'singleForm')).toEqual('secondForm');
+    });
+});
