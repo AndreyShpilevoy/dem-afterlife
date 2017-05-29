2d19a08e92f8d406d36ecce4a6340c3e4d428c21
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-05-29 13:34:03 +0300

    refactored dateTimeDeclension for RuUa zone added localization index.js file

---
+++ src/dem-afterlife-ui/src/scripts/utils/localization/index.js
@@ -0,0 +1,110 @@
+import translations from './translations';
+
+const milliseconds = {
+    perMinute: 60000,
+    perHour: 3600000,
+    perDay: 86400000,
+    perMonth: 2592000000,
+    perYear: 31536000000
+};
+
+const getCurrentTranslations = locale => translations.find(x => x.locale === locale);
+
+export const getTermTranslation = (term, locale) => {
+    if (term && locale) {
+        const currentTranslations = getCurrentTranslations(locale);
+        if (currentTranslations) {
+            const translation = currentTranslations.translationArray.find(x => x.id === term.id);
+            if (translation) {
+                return translation.value;
+            }
+            return term.value;
+        }
+        return term.value;
+    } else if (term) {
+        return term.value;
+    }
+    return 'no term';
+};
+
+export const transformRelativeMoment = (msDeltaTime, locale) => getTermTranslation({id: 5, value: 'less than a minute'}, locale);
+
+export const transformRelativeMinute = (msDeltaTime, locale, dateTimeDeclension) => {
+    const number = Math.round(msDeltaTime / milliseconds.perMinute);
+    const oneMinute = getTermTranslation({id: 6, value: 'about a minute'}, locale);
+    const firstForm = getTermTranslation({id: 7, value: 'minutes'}, locale);
+    const secondForm = getTermTranslation({id: 8, value: 'minutes'}, locale);
+    const thirdForm = getTermTranslation({id: 9, value: 'minutes'}, locale);
+    return dateTimeDeclension(number, firstForm, secondForm, thirdForm, oneMinute);
+};
+
+export const transformRelativeHour = (msDeltaTime, locale, dateTimeDeclension) => {
+    const number = Math.round(msDeltaTime / milliseconds.perHour);
+    const firstForm = getTermTranslation({id: 10, value: 'about an hour'}, locale);
+    const secondForm = getTermTranslation({id: 11, value: 'hours'}, locale);
+    const thirdForm = getTermTranslation({id: 12, value: 'hours'}, locale);
+    return dateTimeDeclension(number, firstForm, secondForm, thirdForm, firstForm);
+};
+
+export const transformRelativeDay = (msDeltaTime, locale, dateTimeDeclension) => {
+    const number = Math.round(msDeltaTime / milliseconds.perDay);
+    const firstForm = getTermTranslation({id: 13, value: 'a day'}, locale);
+    const secondForm = getTermTranslation({id: 14, value: 'days'}, locale);
+    const thirdForm = getTermTranslation({id: 15, value: 'days'}, locale);
+    return dateTimeDeclension(number, firstForm, secondForm, thirdForm, firstForm);
+};
+
+export const transformRelativeMonth = (msDeltaTime, locale, dateTimeDeclension) => {
+    const number = Math.round(msDeltaTime / milliseconds.perMonth);
+    const firstForm = getTermTranslation({id: 16, value: 'about a month'}, locale);
+    const secondForm = getTermTranslation({id: 17, value: 'months'}, locale);
+    const thirdForm = getTermTranslation({id: 18, value: 'months'}, locale);
+    return dateTimeDeclension(number, firstForm, secondForm, thirdForm, firstForm);
+};
+
+export const transformRelativeYear = (msDeltaTime, locale, dateTimeDeclension) => {
+    const number = Math.round(msDeltaTime / milliseconds.perYear);
+    const firstForm = getTermTranslation({id: 19, value: 'about a year'}, locale);
+    const secondForm = getTermTranslation({id: 20, value: 'years'}, locale);
+    const thirdForm = getTermTranslation({id: 21, value: 'years'}, locale);
+    return dateTimeDeclension(number, firstForm, secondForm, thirdForm, firstForm);
+};
+
+/* eslint "complexity": [1, 6]*/
+export const getTransformForPeriod = msDeltaTime => {
+    if (msDeltaTime < milliseconds.perMinute) {
+        return transformRelativeMoment;
+    } else if (msDeltaTime < milliseconds.perHour) {
+        return transformRelativeMinute;
+    } else if (msDeltaTime < milliseconds.perDay) {
+        return transformRelativeHour;
+    } else if (msDeltaTime < milliseconds.perMonth) {
+        return transformRelativeDay;
+    } else if (msDeltaTime < milliseconds.perYear) {
+        return transformRelativeMonth;
+    }
+    return transformRelativeYear;
+};
+
+export const getRelativeDateTime = (date, locale) => {
+    if (!date) {
+        return 'date is not defined';
+    }
+
+    const currentTranslations = getCurrentTranslations(locale);
+    if (!currentTranslations) {
+        return `${date.toLocaleDateString()} ${date.getHours()}:${date.getMinutes()}`;
+    }
+
+    const suffixAgo = getTermTranslation({id: 4, value: 'ago'}, locale);
+    const msDeltaTime = new Date() - date;
+
+    return `${getTransformForPeriod()(msDeltaTime, locale, currentTranslations.dateTimeDeclension)} ${suffixAgo}`;
+};
+
+export const getLocaleDateTime = (date, locale, options) => {
+    if (!date) {
+        return 'date is not defined';
+    }
+    return date.toLocaleTimeString(locale, options);
+};
--- src/dem-afterlife-ui/src/scripts/utils/localization/translations/dateTimeDeclensions/ruUa.js
+++ src/dem-afterlife-ui/src/scripts/utils/localization/translations/dateTimeDeclensions/ruUa.js
@@ -1,17 +1,17 @@
-export default function dateTimeDeclension(number, firstForm, secondForm, trirdForm, singleForm) {
+const dateTimeDeclension = (number, firstForm, secondForm, trirdForm, singleForm) => {
     // singleForm - 1
     // firstForm - 21, 31, ...
     // secondForm - 2-4, 22-24, 32-34 ...
     // trirdForm - 5-20, 25-30, ...
-  let remainder = number % 10;
-  if (number === 1) {
-    return singleForm;
-  }
-  else if ((remainder === 1) && ((number > 20))) {
-    return `${number} ${firstForm}`;
-  } else if ((remainder > 1) && (remainder < 5) && ((number > 20) || (number < 10))) {
-    return `${number} ${secondForm}`;
-  } else {
+    const remainder = number % 10;
+    if (number === 1) {
+        return singleForm;
+    } else if (remainder === 1 && number > 20) {
+        return `${number} ${firstForm}`;
+    } else if (remainder > 1 && remainder < 5 && (number > 20 || number < 10)) {
+        return `${number} ${secondForm}`;
+    }
     return `${number} ${trirdForm}`;
-  }
-}
+};
+
+export default dateTimeDeclension;
