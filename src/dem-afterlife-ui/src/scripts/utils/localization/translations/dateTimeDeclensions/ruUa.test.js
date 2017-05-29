c40936f8d060da3a980c7484783d8a160af8d1cc
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-05-29 23:31:42 +0300

    added tests for all dateTimeDeclension

--- src/dem-afterlife-ui/src/scripts/utils/localization/translations/dateTimeDeclensions/ruUa.test.js
+++ src/dem-afterlife-ui/src/scripts/utils/localization/translations/dateTimeDeclensions/ruUa.test.js
@@ -1,13 +1,21 @@
 /* eslint no-undef: 0, fp/no-unused-expression: 0, fp/no-nil: 0*/
 
-import dateTimeDeclension from './en';
+import dateTimeDeclension from './ruUa';
 
 describe('EN dateTimeDeclension', () => {
     it('dateTimeDeclension return singleForm string for 1 item', () => {
         expect(dateTimeDeclension(1, 'firstForm', 'secondForm', 'trirdForm', 'singleForm')).toEqual('singleForm');
     });
 
+    it('dateTimeDeclension return firstForm string for 21 item', () => {
+        expect(dateTimeDeclension(21, 'firstForm', 'secondForm', 'trirdForm', 'singleForm')).toEqual('21 firstForm');
+    });
+
     it('dateTimeDeclension return secondForm string for 2 item', () => {
         expect(dateTimeDeclension(2, 'firstForm', 'secondForm', 'trirdForm', 'singleForm')).toEqual('2 secondForm');
+    });
+
+    it('dateTimeDeclension return trirdForm string for 5 item', () => {
+        expect(dateTimeDeclension(5, 'firstForm', 'secondForm', 'trirdForm', 'singleForm')).toEqual('5 trirdForm');
     });
 });
