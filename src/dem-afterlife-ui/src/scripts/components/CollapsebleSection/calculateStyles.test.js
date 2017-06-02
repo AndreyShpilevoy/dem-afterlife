e8eeedbcee5b495233de7351324f2eb31646320f
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-06-03 00:25:46 +0300

    added test for CollapsebleSection calculateStyles

--- src/dem-afterlife-ui/src/scripts/components/CollapsebleSection/calculateStyles.test.js
+++ src/dem-afterlife-ui/src/scripts/components/CollapsebleSection/calculateStyles.test.js
@@ -3,7 +3,44 @@
 import calculateStyles, {
 } from './calculateStyles';
 
-describe('Header calculateStyles', () => {
+describe('CollapsebleSection calculateStyles', () => {
+    const defaultThemeObject = {
+        grid: {
+            containers: [
+                {
+                    gridSize: 'xs',
+                    mediaMinString: 'min-width: 0px',
+                    mediaMaxString: 'max-width: 575px',
+                    width: '100%'
+                },
+                {
+                    gridSize: 'xl',
+                    mediaMinString: 'min-width: 1200px',
+                    mediaMaxString: 'max-width: 100vw',
+                    width: '1140px'
+                }
+            ]
+        },
+        collapsebleSection: {
+            general: {
+                marginTop: 'sizes.marginBetweenBlocks',
+                padding: 'sizes.horizontalPadingAndMargin'
+            },
+            header: {
+                backgroundColor: 'colors.collapsebleSectionHeaderBackgroundColor',
+                color: 'colors.orangeColor',
+                height: 2.5,
+                fontSize: 1.25
+            },
+            body: {
+                backgroundColor: 'colors.collapsebleSectionBodyBackgroundColor',
+                color: 'colors.contentHolderBodyTextColor',
+                transition: 'linearTransition'
+            }
+        }
+    };
+
     it('should create expected object', () => {
+        expect(calculateStyles(defaultThemeObject)).toMatchSnapshot();
     });
 });
