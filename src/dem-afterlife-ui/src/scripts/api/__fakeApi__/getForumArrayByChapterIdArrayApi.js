9b9bb697708a1b18560624f3c113bf1e38fb4418
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-06-06 14:14:59 +0300

    fixed wrong Fake API for GetForumByChapterIdArray

--- src/dem-afterlife-ui/src/scripts/api/__fakeApi__/getForumArrayByChapterIdArrayApi.js
+++ src/dem-afterlife-ui/src/scripts/api/__fakeApi__/getForumArrayByChapterIdArrayApi.js
@@ -11,25 +11,25 @@ const getForumArrayByChapterIdArrayApi = chapterIdArray => {
             case 10:
             case 11:
             case 12:
-                result.push({...{chapterId}, ...forums[0]});
-                result.push({...{chapterId}, ...forums[1]});
+                result.push({...forums[0], ...{chapterId} });
+                result.push({...forums[1], ...{chapterId} });
                 break;
             case 2:
             case 20:
-                result.push({...{chapterId}, ...forums[1]});
-                result.push({...{chapterId}, ...forums[0]});
+                result.push({...forums[1], ...{chapterId} });
+                result.push({...forums[0], ...{chapterId} });
                 break;
             case 3:
             case 30:
             case 31:
-                result.push({...{chapterId}, ...forums[1]});
+                result.push({...forums[1], ...{chapterId} });
                 break;
             case 4:
             case 40:
-                result.push({...{chapterId}, ...forums[0]});
-                result.push({...{chapterId}, ...forums[1]});
-                result.push({...{chapterId}, ...forums[2]});
-                result.push({...{chapterId}, ...forums[3]});
+                result.push({...forums[0], ...{chapterId} });
+                result.push({...forums[1], ...{chapterId} });
+                result.push({...forums[2], ...{chapterId} });
+                result.push({...forums[3], ...{chapterId} });
                 break;
             case 5:
             case 50:
@@ -39,9 +39,9 @@ const getForumArrayByChapterIdArrayApi = chapterIdArray => {
             case 54:
             case 55:
             case 56:
-                result.push({...{chapterId}, ...forums[0]});
-                result.push({...{chapterId}, ...forums[1]});
-                result.push({...{chapterId}, ...forums[4]});
+                result.push({...forums[0], ...{chapterId} });
+                result.push({...forums[1], ...{chapterId} });
+                result.push({...forums[4], ...{chapterId} });
                 break;
             default:
         }
