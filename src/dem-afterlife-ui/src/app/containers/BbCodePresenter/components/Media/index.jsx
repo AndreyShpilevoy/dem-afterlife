05b7164e2e01fc3bc887781066116cada71a2825
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-09-11 18:35:17 +0300

    added propTypes to Media nested presenters

--- src/dem-afterlife-ui/src/app/containers/BbCodePresenter/components/Media/index.jsx
+++ src/dem-afterlife-ui/src/app/containers/BbCodePresenter/components/Media/index.jsx
@@ -1,21 +1,54 @@
+/* eslint-disable jsx-a11y/media-has-caption */
+
 import React from 'react';
-import {node, shape} from 'prop-types';
+import {node, shape, number, bool, string} from 'prop-types';
 import {injectSheet} from 'styles';
 import Term from 'containers/Term';
 import parseFirstTextLineComponentToEmbedLink from './parser';
 import calculateStyles from './calculateStyles';
 
-const iframePresenter = ({height, width, src, shortHeight}) => // eslint-disable-line react/prop-types
+const iframePresenter = ({height, width, src, shortHeight}) =>
     <iframe title='This is a unique title' width={width} height={shortHeight ? 100 : height} src={src} allowFullScreen frameBorder='0' />;
 
-const videoPresenter = ({height, width, src, type}) => { // eslint-disable-line react/prop-types
-    const videoTerm = {id: 29, value: 'Video'};
-    return <video width={width} height={height} controls><source src={src} type={type} /><a href={src}><Term term={videoTerm} /></a></video>; // eslint-disable-line jsx-a11y/media-has-caption
+iframePresenter.propTypes = {
+    height: number.isRequired,
+    width: number.isRequired,
+    shortHeight: bool.isRequired,
+    src: string.isRequired
 };
 
-const audioPresenter = ({src, type}) => { // eslint-disable-line react/prop-types
+const videoPresenter = ({height, width, src, type}) => {
+    const videoTerm = {id: 29, value: 'Video'};
+    return (
+        <video width={width} height={height} controls>
+            <source src={src} type={type} />
+            <a href={src}>
+                <Term term={videoTerm} />
+            </a>
+        </video>
+    );
+};
+
+videoPresenter.propTypes = {
+    height: number.isRequired,
+    width: number.isRequired,
+    type: string.isRequired,
+    src: string.isRequired
+};
+
+const audioPresenter = ({src, type}) => {
     const audioTerm = {id: 28, value: 'Audio'};
-    return <audio controls><source src={src} type={type} /><a href={src}><Term term={audioTerm} /></a></audio>; // eslint-disable-line jsx-a11y/media-has-caption
+    return (
+        <audio controls>
+            <source src={src} type={type} />
+            <a href={src}><Term term={audioTerm} /></a>
+        </audio>
+    );
+};
+
+audioPresenter.propTypes = {
+    type: string.isRequired,
+    src: string.isRequired
 };
 
 
