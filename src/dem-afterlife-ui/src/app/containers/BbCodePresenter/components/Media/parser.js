63649f5ea8dd6a8fb1c27dd03ab4fc5b8baa6135
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-09-14 21:03:40 +0300

    refactored parseTextLineComponentToEmbedLink to avoide a lot of repetable code

--- src/dem-afterlife-ui/src/app/containers/BbCodePresenter/components/Media/parser.js
+++ src/dem-afterlife-ui/src/app/containers/BbCodePresenter/components/Media/parser.js
@@ -11,68 +11,39 @@ import yandexMusicAudio from './parsers/yandexMusicAudio';
 import youtubePlaylist from './parsers/youtubePlaylist';
 import youtubeVideo from './parsers/youtubeVideo';
 
+const listOfParsers = [
+    coubVideo,
+    facebookVideo,
+    googleMaps,
+    HTML5Tag,
+    soundCloudAudio,
+    twitchVideo,
+    vimeoVideo,
+    vkVideo,
+    yandexMusicAudio,
+    youtubePlaylist,
+    youtubeVideo
+];
+
 const parseTextLineComponentToEmbedLink = textLineComponent => {
     if (textLineComponent.type.displayName === TextLine.displayName) {
-        const youtubePlaylistResult = youtubePlaylist(textLineComponent.props.children);
-        if (youtubePlaylistResult.success) {
-            return youtubePlaylistResult;
-        }
-
-        const youtubeVideoResult = youtubeVideo(textLineComponent.props.children);
-        if (youtubeVideoResult.success) {
-            return youtubeVideoResult;
-        }
-
-        const vimeoVideoResult = vimeoVideo(textLineComponent.props.children);
-        if (vimeoVideoResult.success) {
-            return vimeoVideoResult;
-        }
-
-        const vkVideoResult = vkVideo(textLineComponent.props.children);
-        if (vkVideoResult.success) {
-            return vkVideoResult;
-        }
-
-        const facebookVideoResult = facebookVideo(textLineComponent.props.children);
-        if (facebookVideoResult.success) {
-            return facebookVideoResult;
-        }
-
-        const twitchVideoResult = twitchVideo(textLineComponent.props.children);
-        if (twitchVideoResult.success) {
-            return twitchVideoResult;
-        }
-
-        const coubVideoResult = coubVideo(textLineComponent.props.children);
-        if (coubVideoResult.success) {
-            return coubVideoResult;
-        }
-
-        const soundCloudAudioResult = soundCloudAudio(textLineComponent.props.children);
-        if (soundCloudAudioResult.success) {
-            return soundCloudAudioResult;
-        }
-
-        const yandexMusicAudioResult = yandexMusicAudio(textLineComponent.props.children);
-        if (yandexMusicAudioResult.success) {
-            return yandexMusicAudioResult;
-        }
-
-        const googleMapsResult = googleMaps(textLineComponent.props.children);
-        if (googleMapsResult.success) {
-            return googleMapsResult;
-        }
-
-        const HTML5TagResult = HTML5Tag(textLineComponent.props.children);
-        if (HTML5TagResult.success) {
-            return HTML5TagResult;
-        }
+        const text = textLineComponent.props.children;
+        const result = listOfParsers.reduce((previous, current)=>{
+            if (previous.success) {
+                return previous;
+            }
+            const parserResult = current(text);
+            if (parserResult.success) {
+                return parserResult;
+            }
+            return previous;
+        }, {
+            type: 'none',
+            success: false,
+            url: ''
+        });
     }
-    return {
-        type: 'none',
-        success: false,
-        url: ''
-    };
+    return result;
 };
 
 export default parseTextLineComponentToEmbedLink;
