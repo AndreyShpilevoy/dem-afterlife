956f232e2260bcc9b3c474700ef13928ffe8bc5d
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-09-18 23:00:03 +0300

    isolated test for parser updated snapshot

--- src/dem-afterlife-ui/src/app/containers/BbCodePresenter/components/Media/parser.test.js
+++ src/dem-afterlife-ui/src/app/containers/BbCodePresenter/components/Media/parser.test.js
@@ -2,37 +2,35 @@
 
 import React from 'react';
 import TextLine from '../TextLine';
-import parseTextLineComponentToEmbedLink, {defaultListOfParsers} from './parser';
+import parseTextLineComponentToEmbedLink from './parser';
+
+const firstParser = jest.fn(string => string === 'valid' ?
+    {
+        type: 'iframe',
+        success: true,
+        url: 'https://Valid',
+        shortHeight: false
+    } :
+    {success: false});
+
+const secondParser = jest.fn(() => ({success: false}));
+const thirdParser = jest.fn(() => ({success: false}));
 
 describe('Media parseTextLineComponentToEmbedLink', () => {
     it('should return default result if sent wrong first argument', () => {
-        expect(parseTextLineComponentToEmbedLink('TextLine', [defaultListOfParsers[9]])).toMatchSnapshot();
+        expect(parseTextLineComponentToEmbedLink('TextLine', [firstParser])).toMatchSnapshot();
     });
 
     it('should return default result if sent empty array as second argument', () => {
-        expect(parseTextLineComponentToEmbedLink(
-            <TextLine>
-                {'https://www.youtube.com/playlist?list=PL7y5d3VOW3rg6nn0bXii79-92jmLYechP'}
-            </TextLine>,
-            []
-        )).toMatchSnapshot();
+        expect(parseTextLineComponentToEmbedLink(<TextLine>{'valid'}</TextLine>, [])).toMatchSnapshot();
     });
 
     it('should return default result if sent correct arguments, but with wrong data', () => {
-        expect(parseTextLineComponentToEmbedLink(
-            <TextLine>
-                {'some wrong content'}
-            </TextLine>,
-            [defaultListOfParsers[9]]
-        )).toMatchSnapshot();
+        expect(parseTextLineComponentToEmbedLink(<TextLine>{'some wrong content'}</TextLine>, [firstParser])).toMatchSnapshot();
     });
 
     it('should return handled result if data was correct', () => {
-        expect(parseTextLineComponentToEmbedLink(
-            <TextLine>
-                {'https://www.youtube.com/playlist?list=PL7y5d3VOW3rg6nn0bXii79-92jmLYechP'}
-            </TextLine>,
-            [defaultListOfParsers[0], defaultListOfParsers[9], defaultListOfParsers[10]]
-        )).toMatchSnapshot();
+        expect(parseTextLineComponentToEmbedLink(<TextLine>{'valid'}</TextLine>,
+            [secondParser, firstParser, thirdParser])).toMatchSnapshot();
     });
 });
