faa44729e7ad08e1da16349f6c73225d4d57388c
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-10-29 16:45:44 +0200

    finished with tests for pagination

--- src/dem-afterlife-ui/src/app/components/PaginationList/createPaginationList.test.js
+++ src/dem-afterlife-ui/src/app/components/PaginationList/createPaginationList.test.js
@@ -9,32 +9,32 @@ describe('PaginationList arrayGenerator', () => {
     });
     it('createPaginationList (default) when paginationItemsCount < maxListLength should call function generateFullPaginationList', () => {
         paginationListCreators.generateFullPaginationList = jest.fn();
-        createPaginationList(2, 1, 3, 0);
+        createPaginationList(9, 2, 10, 1);
         expect(paginationListCreators.generateFullPaginationList.mock.calls.length).toEqual(1);
     });
     it('createPaginationList (default) when paginationItemsCount = maxListLength should call function generateFullPaginationList', () => {
         paginationListCreators.generateFullPaginationList = jest.fn();
-        createPaginationList(2, 2, 2, 0);
+        createPaginationList(10, 2, 10, 1);
         expect(paginationListCreators.generateFullPaginationList.mock.calls.length).toEqual(1);
     });
     it('createPaginationList (default) when pageNumber < offsetListLength + 1 should call function generateStartPaginationList', () => {
         paginationListCreators.generateStartPaginationList = jest.fn();
-        createPaginationList(5, 5, 1, 5);
+        createPaginationList(20, 1, 9, 6);
         expect(paginationListCreators.generateStartPaginationList.mock.calls.length).toEqual(1);
     });
     it('createPaginationList (default) when pageNumber < paginationItemsCount - offsetListLength should call function generateMiddlePaginationList', () => {
         paginationListCreators.generateMiddlePaginationList = jest.fn();
-        createPaginationList(5, 3, 1, 1);
+        createPaginationList(20, 8, 9, 6);
         expect(paginationListCreators.generateMiddlePaginationList.mock.calls.length).toEqual(1);
     });
     it('createPaginationList (default) when pageNumber = paginationItemsCount - offsetListLength should call function generateMiddlePaginationList', () => {
         paginationListCreators.generateMiddlePaginationList = jest.fn();
-        createPaginationList(5, 3, 1, 2);
+        createPaginationList(20, 14, 9, 6);
         expect(paginationListCreators.generateMiddlePaginationList.mock.calls.length).toEqual(1);
     });
     it('createPaginationList (default) should call function generateEndPaginationList', () => {
         paginationListCreators.generateEndPaginationList = jest.fn();
-        createPaginationList(21, 19, 10, 3);
+        createPaginationList(20, 20, 9, 6);
         expect(paginationListCreators.generateEndPaginationList.mock.calls.length).toEqual(1);
     });
 });
---
+++ src/dem-afterlife-ui/src/app/components/PaginationList/paginationListCreators.test.js
@@ -0,0 +1,22 @@
+/* eslint no-undef: 0, fp/no-unused-expression: 0, fp/no-mutation: 0, fp/no-nil: 0, import/no-duplicates: 0,react/jsx-filename-extension:0 */
+import {
+    generateFullPaginationList,
+    generateStartPaginationList,
+    generateMiddlePaginationList,
+    generateEndPaginationList
+} from './paginationListCreators';
+
+describe('PaginationList creator', () => {
+    it('generateFullPaginationList should match expected snapshot', () => {
+        expect(generateFullPaginationList(9, 2, 10, 1)).toMatchSnapshot();
+    });
+    it('generateStartPaginationList should match expected snapshot', () => {
+        expect(generateStartPaginationList(20, 1, 9, 6)).toMatchSnapshot();
+    });
+    it('generateMiddlePaginationList should match expected snapshot', () => {
+        expect(generateMiddlePaginationList(20, 8, 9, 6)).toMatchSnapshot();
+    });
+    it('generateEndPaginationList should match expected snapshot', () => {
+        expect(generateEndPaginationList(20, 20, 9, 6)).toMatchSnapshot();
+    });
+});
