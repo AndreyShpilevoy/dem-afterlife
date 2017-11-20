34dba6267adccce45f1e333aef223273e4001e81
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2017-11-20 18:24:53 +0200

    updated npm packages fixed eslint errors

--- src/dem-afterlife-ui/src/app/containers/RelativeDateTime/index.jsx
+++ src/dem-afterlife-ui/src/app/containers/RelativeDateTime/index.jsx
@@ -1,46 +1,44 @@
 /* eslint fp/no-class: 0, fp/no-nil: 0, fp/no-unused-expression: 0, fp/no-this: 0 */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {instanceOf, string, bool} from 'prop-types';
 import {connect} from 'react-redux';
 import {localization} from 'utils';
 import {localeSelector} from 'containers/selectors';
 
-class RelativeDateTime extends Component {
-    static propTypes = {
-        relativeDateTime: instanceOf(Date).isRequired,
-        locale: string.isRequired,
-        className: string,
-        spaceBefore: bool,
-        spaceAfter: bool,
-        indentBefore: bool,
-        indentAfter: bool
+const RelativeDateTime = ({relativeDateTime, locale, className, spaceBefore, spaceAfter, indentBefore, indentAfter}) => {
+    const spaceBeforeString = spaceBefore ? ' ' : '';
+    const spaceAfterString = spaceAfter ? ' ' : '';
+    const content = `${spaceBeforeString}${localization.getRelativeDateTime(relativeDateTime, locale)}${spaceAfterString}`;
+    const style = {
+        paddingLeft: indentBefore ? '0.3125rem' : '',
+        paddingRight: indentAfter ? '0.3125rem' : ''
     };
+    return (
+        <span className={className} style={style}>
+            {content}
+        </span>
+    );
+};
 
-    static defaultProps = {
-        className: '',
-        spaceBefore: false,
-        spaceAfter: false,
-        indentBefore: false,
-        indentAfter: false
-    };
 
-    render() {
-        const {relativeDateTime, locale, className, spaceBefore, spaceAfter, indentBefore, indentAfter} = this.props;
-        const spaceBeforeString = spaceBefore ? ' ' : '';
-        const spaceAfterString = spaceAfter ? ' ' : '';
-        const content = `${spaceBeforeString}${localization.getRelativeDateTime(relativeDateTime, locale)}${spaceAfterString}`;
-        const style = {
-            paddingLeft: indentBefore ? '0.3125rem' : '',
-            paddingRight: indentAfter ? '0.3125rem' : ''
-        };
-        return (
-            <span className={className} style={style}>
-                {content}
-            </span>
-        );
-    }
-}
+RelativeDateTime.propTypes = {
+    relativeDateTime: instanceOf(Date).isRequired,
+    locale: string.isRequired,
+    className: string,
+    spaceBefore: bool,
+    spaceAfter: bool,
+    indentBefore: bool,
+    indentAfter: bool
+};
+
+RelativeDateTime.defaultProps = {
+    className: '',
+    spaceBefore: false,
+    spaceAfter: false,
+    indentBefore: false,
+    indentAfter: false
+};
 
 const mapStateToProps = state => ({
     locale: localeSelector(state)
--- src/dem-afterlife-ui/src/app/containers/Term/index.jsx
+++ src/dem-afterlife-ui/src/app/containers/Term/index.jsx
@@ -1,55 +1,52 @@
 /* eslint fp/no-class: 0, fp/no-nil: 0, fp/no-unused-expression: 0, fp/no-this: 0 */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {shape, number, string, bool} from 'prop-types';
 import {connect} from 'react-redux';
 import {localization} from 'utils';
 import {localeSelector} from 'containers/selectors';
 
-class Term extends Component {
-    static propTypes = {
-        term: shape({
-            id: number.isRequired,
-            value: string.isRequired
-        }),
-        className: string,
-        spaceBefore: bool,
-        spaceAfter: bool,
-        indentBefore: bool,
-        indentAfter: bool,
-        untermedPostfix: string,
-        locale: string.isRequired,
-        doNotTerm: bool
+const Term = ({term, locale, className, spaceBefore, spaceAfter, indentBefore, indentAfter, untermedPostfix, doNotTerm}) => {
+    const spaceBeforeString = spaceBefore ? ' ' : '';
+    const spaceAfterString = spaceAfter ? ' ' : '';
+    const termedString = doNotTerm ? '' : localization.getTermTranslation(term, locale);
+    const content = `${spaceBeforeString}${termedString}${untermedPostfix}${spaceAfterString}`;
+    const style = {
+        paddingLeft: indentBefore ? '0.25rem' : '',
+        paddingRight: indentAfter ? '0.25rem' : ''
     };
+    return (
+        <span className={className} style={style}>
+            {content}
+        </span>
+    );
+};
 
-    static defaultProps = {
-        term: null,
-        untermedPostfix: '',
-        doNotTerm: false,
-        className: '',
-        spaceBefore: false,
-        spaceAfter: false,
-        indentBefore: false,
-        indentAfter: false
-    };
+Term.propTypes = {
+    term: shape({
+        id: number.isRequired,
+        value: string.isRequired
+    }),
+    className: string,
+    spaceBefore: bool,
+    spaceAfter: bool,
+    indentBefore: bool,
+    indentAfter: bool,
+    untermedPostfix: string,
+    locale: string.isRequired,
+    doNotTerm: bool
+};
 
-    render() {
-        const {term, locale, className, spaceBefore, spaceAfter, indentBefore, indentAfter, untermedPostfix, doNotTerm} = this.props;
-        const spaceBeforeString = spaceBefore ? ' ' : '';
-        const spaceAfterString = spaceAfter ? ' ' : '';
-        const termedString = doNotTerm ? '' : localization.getTermTranslation(term, locale);
-        const content = `${spaceBeforeString}${termedString}${untermedPostfix}${spaceAfterString}`;
-        const style = {
-            paddingLeft: indentBefore ? '0.25rem' : '',
-            paddingRight: indentAfter ? '0.25rem' : ''
-        };
-        return (
-            <span className={className} style={style}>
-                {content}
-            </span>
-        );
-    }
-}
+Term.defaultProps = {
+    term: null,
+    untermedPostfix: '',
+    doNotTerm: false,
+    className: '',
+    spaceBefore: false,
+    spaceAfter: false,
+    indentBefore: false,
+    indentAfter: false
+};
 
 const mapStateToProps = state => ({
     locale: localeSelector(state)
